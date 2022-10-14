e86cf4329b58f3a1e87fc123b783b03305b98099
andybeardness <beardness.andy@gmail.com>
2022-10-14 15:06:06 +0400

    comment

--- double_mod.h
+++ double_mod.h
@@ -1,8 +1,9 @@
-#define MOD_ERROR_RESULT -1.
+#define MOD_ERROR_RESULT -1. /* ERROR STATEMENT */
 
 float float_mod(float initial, float mod);
 double double_mod(double initial, double mod);
 
+/* Mod function for 'float' */
 float float_mod(float initial, float mod)
 {
     if (mod <= 0)       return MOD_ERROR_RESULT;
@@ -15,6 +16,7 @@ float float_mod(float initial, float mod)
     return initial;
 }
 
+/* Mod function for 'double' */
 double double_mod(double initial, double mod)
 {
     if (mod <= 0)       return MOD_ERROR_RESULT;
